3af3b75c4e43a60ede639ac3d7bed8222973366b
vanya-n23 <vanya.neresnitsia@gmail.com>
2025-03-01 13:42:26 +0000

    d

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { initMongoConnection } from './db/initMongoConnection.js';
-import { startServer } from './server.js';
+import { setUpServer } from './server.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
-  startServer();
+  setUpServer();
 };
 
 bootstrap();
